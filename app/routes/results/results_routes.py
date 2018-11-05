0fce7ae98b87f8efd71ef69d72ff1ba066108d29
Wes Evans <euphwes@gmail.com>
2018-11-05 14:49:17 -0700

    Added some quick timestamps

--- app/routes/results/results_routes.py
+++ app/routes/results/results_routes.py
@@ -34,26 +34,40 @@ def prev_leaders():
 def comp_results(comp_id):
     """ A route for showing results for a specific competition. """
 
+    total_start = now()
+
     competition = comp_manager.get_competition(comp_id)
+
+    get_comp_start = now()
     comp_events = comp_manager.get_all_comp_events_for_comp(comp_id)
+    print("Retrieve comp: " + str(now() - get_comp_start))
 
+    get_all_results_start = now()
     results = comp_manager.get_all_complete_user_results_for_comp(comp_id)
+    print("Get all user results for comp " + str(now() - get_all_results_start))
 
-    event_names   = [event.Event.name for event in comp_events]
+    build_lists_start  = now()
+    event_names = [event.Event.name for event in comp_events]
     event_results = {event.Event.name : list() for event in comp_events}
     event_formats = {event.Event.name : event.Event.eventFormat for event in comp_events}
     event_ids     = {event.Event.name : event.Event.id for event in comp_events}
+    print("Build lists: " + str(now() - build_lists_start))
 
+    build_times_str_start = now()
     for result in results:
         event_name = result.CompetitionEvent.Event.name
         solves_helper = result.times_string.split(', ')
         setattr(result, 'solves_helper', solves_helper)
         event_results[event_name].append(result)
+    print("Build time strings: " + str(now() - build_times_str_start))
 
     # Sort the results
+    sort_start = now()
     for event_name, results in event_results.items():
         results.sort(key=cmp_to_key(sort_results))
+    print("Sort: " + str(now() - sort_start))
 
+    print("Everything: " + str(now() - total_start))
     return render_template("results/results_comp.html", comp_name=competition.title,\
         event_results=event_results, event_names=event_names, event_formats=event_formats,\
         event_ids=event_ids)
