950f16fbe7b80fbbd7c1e6da0021d357bb751566
Wes Evans <euphwes@gmail.com>
2019-06-07 13:46:44 -0600

    [#65] Added DB migration to change FMC from Mo3 to Bo3 format, to encourage more participation.

---
+++ migrations/versions/028_adad00000000_update_fmc_bo3.py
@@ -0,0 +1,24 @@
+"""Update FMC to Bo3
+
+Revision ID: adad00000000
+Revises: acac00000000
+Create Date: 2019-06-07 13:33:00.123969
+
+"""
+from app.persistence.models import EventFormat
+from sqlalchemy.sql import table, column
+from sqlalchemy import String
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = 'adad00000000'
+down_revision = 'acac00000000'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    events = table('events', column('name', String), column('eventFormat', String))
+    op.execute(events.update().where(events.c.name==op.inline_literal('FMC')).values({'eventFormat':op.inline_literal('Bo3')}))
+
+def downgrade():
+    pass
