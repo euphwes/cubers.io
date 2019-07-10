4772f52ca781c7b56a1bbea0bed8ffda33d081d5
bryankroesbeek <bryank.b96@gmail.com>
2019-07-10 16:59:10 +0200

    Add config files

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    entry: "./client/app.tsx",
+    output: {
+        filename: "site.js",
+        path: __dirname + "/app/static/js"
+    },
+
+    mode: "development",
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
+    },
+};
