b962495234f6a7c12b4c12fafaeb5aa470823e96
Bryan Kroesbeek <bryank.b96@gmail.com>
2019-07-11 00:05:22 +0200

    Add first part of Header

--- client/header/Header.tsx
+++ client/header/Header.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react'
+import { Link } from 'react-router-dom'
+import * as Api from '../api/api'
 
 type HeaderProps = {
 
 }
 
 type HeaderState = {
-    name: string
+    title: string
+    recordsItems: {
+        wca: { url: string, name: string }[]
+        nonWca: { url: string, name: string }[]
+    } | "loading"
 }
 
 export class Header extends React.Component<HeaderProps, HeaderState> {
@@ -13,11 +19,125 @@ export class Header extends React.Component<HeaderProps, HeaderState> {
         super(props)
 
         this.state = {
-            name: "cubers.io"
+            title: "cubers.io",
+            recordsItems: "loading"
         }
     }
 
+    componentDidMount() {
+        Api.getHeaderInfo()
+            .then(info => this.setState({
+                title: info.title,
+                recordsItems: {
+                    wca: info.records.wca,
+                    nonWca: info.records.nonWca
+                }
+            }))
+    }
+
+    renderRecords() {
+        return <>
+            <a className="nav-link dropdown-toggle py-0" role="button" data-toggle="dropdown" data-submenu href="#">Records</a>
+            <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <div className="dropdown dropright dropdown-submenu">
+                    <button className="dropdown-item dropdown-toggle" type="button">WCA Events</button>
+                    <div className="dropdown-menu">
+                        {this.state.recordsItems === "loading" ? null :
+                            this.state.recordsItems.wca.map((item, id) =>
+                                <Link
+                                    key={id}
+                                    className="dropdown-item slim-nav-item"
+                                    to={item.url}
+                                >{item.name}</Link>
+                            )
+                        }
+                    </div>
+                </div>
+                <div className="dropdown dropright dropdown-submenu">
+                    <button className="dropdown-item dropdown-toggle" type="button">Non-WCA Events</button>
+                    <div className="dropdown-menu">
+                        {this.state.recordsItems === "loading" ? null :
+                            this.state.recordsItems.nonWca.map((item, id) =>
+                                <Link
+                                    key={id}
+                                    className="dropdown-item slim-nav-item"
+                                    to={item.url}
+                                >{item.name}</Link>
+                            )
+                        }
+                    </div>
+                </div>
+                {/* <div className="dropdown-divider"></div>
+                <div className="dropdown dropright dropdown-submenu">
+                    <button className="dropdown-item dropdown-toggle" type="button">Sum of Ranks</button>
+                    <div className="dropdown-menu">
+                        <a className="dropdown-item slim-nav-item" href="{{ url_for('sum_of_ranks', sor_type='all') }}">Combined</a>
+                        <a className="dropdown-item slim-nav-item" href="{{ url_for('sum_of_ranks', sor_type='wca') }}">WCA Events Only</a>
+                        <a className="dropdown-item slim-nav-item" href="{{ url_for('sum_of_ranks', sor_type='non_wca') }}">Non-WCA Events Only</a>
+                    </div>
+                </div> */}
+            </div>
+        </>
+    }
+
+    renderLeaderboards() {
+        return <>
+            {/* <a className="nav-link dropdown-toggle py-0" role="button" data-toggle="dropdown" href="#">Leaderboards</a>
+            <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="{{ url_for('curr_leaders') }}">Current Competition</a>
+                <a className="dropdown-item" href="{{ url_for('prev_leaders') }}">Last Week's Competition</a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="{{ url_for('results_list') }}">All competitions</a>
+            </div> */}
+        </>
+    }
+
+    renderUser() {
+        return <>
+            {/* <a className="nav-link dropdown-toggle py-0" role="button" data-toggle="dropdown" href="#">
+                {{ current_user.username }}
+        </a>
+            <div className="dropdown-menu dropdown-menu-right">
+                <a className="dropdown-item" href="{{ url_for('profile', username=current_user.username) }}">Profile</a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="{{ url_for('logout') }}">Logout</a>
+            </div> */}
+        </>
+    }
+
     render() {
-        return <div></div>
+        return <div className="navbar navbar-expand-md navbar-dark cubers-navbar">
+            <div className="container-fluid">
+                <Link to="/" className="navbar-brand py-0">{this.state.title}</Link>
+
+                <button className="navbar-toggler py-0" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+
+                <div id="navbarCollapse" className="collapse navbar-collapse py-0">
+                    <ul className="navbar-nav ml-auto py-0">
+                        <li className="nav-item dropdown">{this.renderRecords()}</li>
+                        <li className="nav-item dropdown">{this.renderLeaderboards()}</li>
+                        <li className="nav-item dropdown">{this.renderUser()}</li>
+
+                        {/* {% if current_user.is_authenticated %} */}
+
+                        <li className="nav-item nav-settings">
+                            <a className="nav-link py-0" href="{{ url_for('edit_settings')}}">
+                                <i className="fas fa-cog"></i>
+                            </a>
+                        </li>
+                        {/* {% else %} */}
+                        {/* <li className="nav-item">
+                    <a className="nav-link py-0" href="{{ url_for('login') }}">Login with Reddit</a>
+                </li> */}
+                        {/* {% endif %} */}
+                    </ul>
+                </div>
+
+
+            </div>
+        </div>
     }
 }
