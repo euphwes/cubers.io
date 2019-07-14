73c470d412d9b2357c85a1fdf7b480b48e8c1393
Bryan Kroesbeek <bryank.b96@gmail.com>
2019-07-14 23:22:48 +0200

    Add routes

--- client/api/api.ts
+++ client/api/api.ts
@@ -6,6 +6,25 @@ function fetchResources<T>(url: string): Promise<T> {
         .then(json => json as T)
 }
 
+function postResources<T>(url: string, data: any) {
+    let headers = new Headers()
+    headers.append('Content-Type', 'application/json')
+
+    let request: RequestInit = {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(data),
+    }
+
+    return fetch(url, request)
+        .then(res => res.json())
+        .then(json => json as T)
+}
+
+export function getUserSettings(): Promise<types.UserSettings> {
+    return fetchResources('/api/user-settings')
+}
+
 export function getHeaderInfo(): Promise<types.HeaderInfo> {
     return fetchResources("/api/header-info")
 }
--- client/router.tsx
+++ client/router.tsx
@@ -3,16 +3,43 @@ import { Route, Switch } from 'react-router'
 import { Header } from './Components/Header/Header';
 import { BrowserRouter } from 'react-router-dom';
 import { Home } from './Components/Home/Home';
+import { Compete } from './Components/Compete/Compete';
 
-export class MainRouter extends React.Component<{}, {}> {
+import * as Api from './api/api'
+import * as Types from './api/types'
+
+type RouterState = {
+    settings: Types.UserSettings | "loading"
+}
+
+type RouterProps = {}
+
+export class MainRouter extends React.Component<RouterProps, RouterState> {
+    constructor(props: RouterProps) {
+        super(props)
+
+        this.state = {
+            settings: "loading"
+        }
+    }
+
+    componentDidMount() {
+        Api.getUserSettings()
+            .then(settings => this.setState({ settings: settings }))
+    }
+
     render() {
+        if (this.state.settings === "loading") return null
+        
         return <BrowserRouter>
             <Header />
 
             <Switch>
                 <Route exact path="/" component={() => <Home />} />
 
-                <Route path="/compete" component={() => <div>Compete</div>} />
+                <Route path="/compete/:eventType" component={({ match }: any) =>
+                    <Compete eventType={Number(match.params.eventType)} />
+                } />
                 <Route path="/event" component={() => <div>Event</div>} />
             </Switch>
         </BrowserRouter>
