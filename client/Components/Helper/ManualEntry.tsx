50a5a74374f4d0079d5bd8d751f2362173ea47e5
Bryan Kroesbeek <bryank.b96@gmail.com>
2019-07-20 14:39:29 +0200

    Finish manual entry component

--- client/Components/Helper/ManualEntry.tsx
+++ client/Components/Helper/ManualEntry.tsx
@@ -17,16 +17,14 @@ export class ManualEntry extends React.Component<ManualEntryProps, ManualEntrySt
     }
 
     processChange(value: string) {
-        let cleanValue = this.cleanInput(value)
-        let numberValue = Number(cleanValue)
+        let numberValue = Number(this.cleanInput(value))
         if (Number.isNaN(numberValue)) return
-
         if (numberValue === 0) return this.setState({ value: "" })
 
-        let goodValue = `${numberValue}`
+        let cleanValue = `${numberValue}`
 
-        let newValue = goodValue.length < 4 ? ("0000" + goodValue).slice(-4) : goodValue
-        if (goodValue.length > 4) {
+        let newValue = cleanValue.length < 4 ? ("0000" + cleanValue).slice(-4) : cleanValue
+        if (cleanValue.length > 4) {
             newValue = newValue.slice(0, -4) + ':' + newValue.slice(-4)
         }
         newValue = newValue.slice(0, -2) + '.' + newValue.slice(-2)
@@ -37,24 +35,22 @@ export class ManualEntry extends React.Component<ManualEntryProps, ManualEntrySt
         return value.replace(".", "").replace(",", "").replace(":", "")
     }
 
-    convertToCentiseconds(value: string) {
+    convertToMilliseconds(value: string) {
         if (value.length <= 5) return Number(value) * 100
 
-        let minutes = Number(value.includes(':') ? value.split(':')[0] : 0) * 60
-        let centiseconds = Math.round((Number(value.includes(':') ? value.split(':')[1] : value) + minutes) * 100)
+        let seconds = Number(value.includes(':') ? value.split(':')[0] : 0) * 60
+        let milliSeconds = Math.round((Number(value.includes(':') ? value.split(':')[1] : value) + seconds) * 1000)
 
-        return centiseconds
+        return milliSeconds
     }
 
     render() {
         return <form onSubmit={e => {
             e.preventDefault()
             if (this.state.value === "") return
-            this.props.submit(this.convertToCentiseconds(this.state.value))
+            this.props.submit(this.convertToMilliseconds(this.state.value))
         }}>
-            <input type="text" value={this.state.value} onChange={e => {
-                this.processChange(e.target.value)
-            }} />
+            <input type="text" value={this.state.value} onChange={e => this.processChange(e.target.value)} />
             <button type="submit">
                 <i className="fas fa-arrow-right" />
             </button>
