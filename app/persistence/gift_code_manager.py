68f747f5285ebccec9260bbc94ff0f9f7684f07a
Wes Evans <euphwes@gmail.com>
2020-10-14 11:02:50 -0600

    [#135] Added persistence module for SCS gift codes.

---
+++ app/persistence/gift_code_manager.py
@@ -0,0 +1,42 @@
+""" Utility module for adding, retrieving, and updating SCS gift codes in the code pool. """
+
+from app import DB
+from app.persistence.models import SCSGiftCodePool
+
+# -------------------------------------------------------------------------------------------------
+
+def bulk_add_gift_codes(gift_codes):
+    """ Add SCS gift codes to the database. """
+
+    for gift_code in gift_codes:
+        code = SCSGiftCodePool(gift_code=gift_code, used=False)
+        DB.session.add(code)
+
+    DB.session.commit()
+
+
+def get_unused_gift_code():
+    """ Retrieves an unused gift code. """
+
+    return DB.session.\
+        query(SCSGiftCodePool).\
+        filter(SCSGiftCodePool.used.is_(False)).\
+        first()
+
+
+def get_unused_gift_code_count():
+    """ Returns the number of unused gift codes remaining in the database. """
+
+    return DB.session.query(SCSGiftCodePool).\
+        filter(SCSGiftCodePool.used.is_(False)).\
+        count()
+
+
+def mark_gift_code_used(gift_code_id):
+    """ Marks the gift code with the supplied ID as used and saves the record. """
+
+    gift_code = SCSGiftCodePool.query.get(gift_code_id)
+    gift_code.used = True
+
+    DB.session.add(gift_code)
+    DB.session.commit()
