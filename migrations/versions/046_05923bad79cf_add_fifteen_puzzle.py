0a8d34f5e337f95b50973099cc327f2bda24091e
Wes Evans <euphwes@gmail.com>
2020-10-23 10:13:45 -0600

    [#140] Added 15 Puzzle DB migration.

---
+++ migrations/versions/046_05923bad79cf_add_fifteen_puzzle.py
@@ -0,0 +1,26 @@
+"""Add Fifteen Puzzle
+
+Revision ID: 05923bad79cf
+Revises: 74b025612a81
+Create Date: 2019-08-23 12:23:00.123969
+
+"""
+from alembic import op
+from app.persistence.models import EventFormat, Event
+
+# revision identifiers, used by Alembic.
+revision = '05923bad79cf'
+down_revision = '74b025612a81'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    op.bulk_insert(
+        Event.__table__,
+        [
+            {'totalSolves': 3, 'eventFormat': EventFormat.Mo3, 'name': '15 Puzzle' },
+        ]
+    )
+
+def downgrade():
+    pass
