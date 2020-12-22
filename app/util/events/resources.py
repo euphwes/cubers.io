b4abe25c35400fb670bfd6de49e718d042ec262c
Wes Evans <euphwes@gmail.com>
2020-12-22 11:50:40 -0700

    [#162] Fix FTO random moves scrambles so that commutative cancellations don't happen. Resolves #162.

--- app/util/events/resources.py
+++ app/util/events/resources.py
@@ -96,23 +96,54 @@ def fifteen_puzzle_scrambler() -> str:
 def fto_scrambler(scramble_length = 30) -> str:
     """ Returns a random-moves scramble of length `scramble_length` for FTO. """
 
-    def __except(n):
-        """ Returns a list range 0-7 except the specified number. """
-        if n == -1:
+    def __except(exceptions):
+        """ Returns a list range 0-7 except the specified exceptions. """
+        if not exceptions:
             return range(8)
         values = list(range(8))
-        values.remove(n)
+        for n in exceptions:
+            values.remove(n)
         return values
 
     moves = ['F', 'U', 'R', 'L', 'D', 'B', 'BL', 'BR']
+    opposites = {
+        'U':  'D',
+        'D':  'U',
+        'F':  'B',
+        'B':  'F',
+        'L':  'BR',
+        'BR': 'L',
+        'BL': 'R',
+        'R':  'BL'
+    }
 
     scramble = list()
     move_ix = -1
 
     for _ in range(scramble_length):
-        # Randomly choose a move, making sure we don't choose the face we just did.
+        if move_ix == -1:
+            # If this is the first move, there are no restrictions on which face we can choose.
+            exceptions = None
+        else:
+            # Otherwise, we'll always omit the face we just chose, so we don't choose it again.
+            exceptions = [move_ix]
+
+            if len(scramble) > 1:
+                # If the last two faces in the scramble were opposites (ex, U and D), the next move
+                # needs to exclude both of those faces so we don't end up with cancellations.
+                # ex: U D U' is no good
+                # ex: R' BL' R' BL is no good
+                # By omitting the two opposite faces from the choices for the next move, the face
+                # chosen will intersect those faces, preventing a cancellation.
+                face_one_ago = scramble[-1].replace("'", "")
+                face_two_ago = scramble[-2].replace("'", "")
+                if opposites[face_one_ago] == face_two_ago:
+                    exceptions.append(moves.index(face_two_ago))
+
+        # Randomly choose a move from the faces available, ensuring we don't have a cancellation
+        move_ix = choice(__except(exceptions))
+
         # Randomly decide if that turn is clockwise or counterclockwise and apply prime if needed.
-        move_ix = choice(__except(move_ix))
         scramble.append(moves[move_ix] + choice(["", "'"]))
 
     return ' '.join(scramble)
