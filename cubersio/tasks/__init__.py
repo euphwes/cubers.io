b2d26d31802fa74b62cb3563847530145adfe0ca
Wes Evans <euphwes@gmail.com>
2021-04-26 10:15:41 -0600

    [#156] Cleaned up tasks __init__ file a little bit.

--- cubersio/tasks/__init__.py
+++ cubersio/tasks/__init__.py
@@ -4,15 +4,16 @@ from huey.signals import SIGNAL_ERROR
 
 from queue_config import huey
 
-from .gift_code_management import *
-from .competition_management import *
-from .reddit import *
-from .scramble_generation import *
+from cubersio import app
+from cubersio.tasks.gift_code_management import *
+from cubersio.tasks.competition_management import *
+from cubersio.tasks.reddit import *
+from cubersio.tasks.scramble_generation import *
 
 
 __TASK_STATUS_REDDIT_USER = app.config['TASK_STATUS_REDDIT_USER']
 
-__TASK_FAILED_BODY  = 'Task {task_name}{task_args} failed due to the following error: {error}'
+__TASK_FAILED_BODY  = 'Task {task_name}({task_args}) failed due to the following error: {error}'
 __TASK_FAILED_TITLE = 'Task {task_name} failed'
 
 
@@ -20,12 +21,10 @@ __TASK_FAILED_TITLE = 'Task {task_name} failed'
 def __task_error_handler(_, task, exc):
     """ Handles when a task fails due to unhandled exceptions. """
 
-    body = __TASK_FAILED_BODY.format(
-        task_name = task.name,
-        task_args = task.args,
-        error     = str(exc)
-    )
+    if app.config['IS_DEVO']:
+        return
 
+    body = __TASK_FAILED_BODY.format(task_name=task.name, task_args=task.args, error=str(exc))
     title = __TASK_FAILED_TITLE.format(task_name=task.name)
 
     send_pm_to_user(__TASK_STATUS_REDDIT_USER, title, body)
